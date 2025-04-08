67ae7998b25c88e80e4b56367c0674ef3f4061f7
chirag tomar <chiragt095@gmail.com>
2025-04-09 00:01:15 +0530

    code update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
-  belongs_to :pan_outlate
+  belongs_to :pan_outlate, optional: true
   belongs_to :state
+
+  OUTLATE_TITLE = [
+    "Minicafe model",
+    "Stand Alone",
+    "Foco model",
+    "Event",
+    "Birthday Celebration",
+    "Wedding Ceremony",
+    "Reunion Party",
+    "Corporate Events",
+    "Inauguration Event",
+    "Engagement Celebration",
+    "Kiosk Mini Cafe Model",
+    "Premium Cafe Franchise",
+    "Foco Model Franchise",
+    "Cafe Model Franchise",
+    "Master Franchise"
+  ]
 end
