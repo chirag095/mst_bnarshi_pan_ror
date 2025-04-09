329f05cf33e33e037137126c3ae217d4315d8772
chirag tomar <chiragt095@gmail.com>
2025-04-09 07:44:23 +0530

    code updste

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,11 @@ class User < ApplicationRecord
   belongs_to :state
 
   OUTLATE_TITLE = [
-    "Minicafe model",
-    "Stand Alone",
-    "Foco model",
-    "Event",
-    "Birthday Celebration",
-    "Wedding Ceremony",
-    "Reunion Party",
-    "Corporate Events",
-    "Inauguration Event",
-    "Engagement Celebration",
-    "Kiosk Mini Cafe Model",
-    "Premium Cafe Franchise",
-    "Foco Model Franchise",
-    "Cafe Model Franchise",
-    "Master Franchise"
+   "kiosk model Franchise",
+   "Cafe model franchise",
+   "Premium model franchise",
+   "Foco model franchise",
+   "Master franchise",
+   "Event"
     ]
 end
